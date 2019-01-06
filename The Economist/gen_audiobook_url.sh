f43cb187a842eefca156408852aae28331e2fe6d
evmn <afls@yandex.com>
2019-01-06 10:42:06 +0800

    There's no 20181229 issue

---
+++ The Economist/gen_audiobook_url.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+#
+#   http://audiocdn.economist.com/sites/default/files/AudioArchive/2018/20180106/Issue_9073_20180106_The_Economist_Full_edition.zip
+#
+
+
+for((i=1;i<=53;i++));do
+    printf "The Economist %02d\n" "$i"
+done
